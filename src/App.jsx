e91674a8636cb64004c652d2b5e9427121949135
dhanushramudri <ramudridhanush@gmail.com>
2024-05-03 00:14:10 +0530

    clerk user details connected to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Account from "./Account";
 import Header from "./components/Header/Header";
+import HomePage from "./components/HomePage/HomePage";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <HomePage />
     </div>
   );
 };
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useUser } from "@clerk/clerk-react";
 
 const HomePage = () => {
-  return <div>Homepage</div>;
+  const { user } = useUser();
+  console.log(user);
+
+  const sendDataToBackend = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/user", {
+        // Adjusted endpoint
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      if (response.ok) {
+        console.log("Data sent to server successfully");
+      } else {
+        console.log("Error in sending data to server");
+      }
+    } catch (error) {
+      console.error("Error sending user data to the backend:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      sendDataToBackend();
+    }
+  }, [user]);
+
+  if (!user) {
+    return <div>Loading user data</div>;
+  }
+
+  return (
+    <div>
+      <div>This is Home page</div>
+      <div>{user.id}</div>
+      <div>Username: {user.username}</div>
+      <div>First Name: {user.firstName}</div>
+      <div>Full Name: {user.fullName}</div>
+      <img
+        style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+        src={user.imageUrl}
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default HomePage;
